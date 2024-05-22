98a9796172914052b86b66945015a9765e6c6be3
Jennie Dalgren <jennie.dalgren@gmail.com>
2024-05-22 16:07:14 +0200

    set up repo

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/flowershop"
+mongoose.connect(mongoUrl)
+mongoose.Promise = Promise
+
+
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+// Start defining your routes here
+// http://localhost:8080/
+app.get("/", (req, res) => {
+  res.send("Hello Technigo!");
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# Frontend part of Final Project
+
+This boilerplate is designed to give you a head start in your React projects, with a focus on understanding the structure and components. As a student of Technigo, you'll find this guide helpful in navigating and utilizing the repository.
+
+## Getting Started
+
+1.  Install the required dependencies using `npm install`.
+2.  Start the development server using `npm run dev`.
