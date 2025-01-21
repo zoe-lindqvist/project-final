08f65545ef990d9e4c594e9dcdbaa605f938a4c0
Zoe <zoe.lindqvist@gmail.com>
2025-01-21 11:18:36 +0100

    create about page and about route

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { About } from "../pages/About"; // Import the About page
 import { Layout } from "../UI/Layout";
 
 export const routes = (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-
       {/* Route for login page */}
-
+      {/* TODO: Add Login Page Route */}
       {/* Route for about page */}
-
+      <Route path="about" element={<About />} /> {/* Add the About route */}
       {/* TODO: Import and Add more routes */}
     </Route>
   </Routes>
