cb9ff7d0ed2557c536cbff22c5dbd2b5da5dead5
JoyceKuode <joyce.kuo@me.com>
2025-01-21 17:28:04 +0100

    Edit User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,22 @@
-const mongoose = require("mongoose");
+import mongoose, { Schema } from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+export default User;
